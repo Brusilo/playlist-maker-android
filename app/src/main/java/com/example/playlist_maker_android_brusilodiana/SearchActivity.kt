f906fb81806187d3af832484201a90c6c61a7455
Brusilo <brusilodi@gmail.com>
2025-11-15 21:44:27 +0300

    Настройка

--- app/src/main/java/com/example/playlist_maker_android_brusilodiana/SearchActivity.kt
+++ app/src/main/java/com/example/playlist_maker_android_brusilodiana/SearchActivity.kt
@@ -16,11 +16,14 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.playlist_maker_android_brusilodiana.ui.theme.MyApplicationTheme
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.ui.text.input.KeyboardType
 
 class SearchActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -41,13 +44,13 @@ fun SearchScreen(onBackClick: () -> Unit) {
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .background(Color.White)
+            .background(colorResource(R.color.white))
     ) {
         TopAppBar(
             title = {
                 Text(
                     stringResource(R.string.search_screen_title),
-                    color = Color.Black,
+                    color = colorResource(R.color.black),
                     fontSize = 20.sp
                 )
             },
@@ -59,7 +62,9 @@ fun SearchScreen(onBackClick: () -> Unit) {
                     )
                 }
             },
-            colors = TopAppBarDefaults.topAppBarColors(containerColor = Color.White)
+            colors = TopAppBarDefaults.topAppBarColors(
+                containerColor = colorResource(R.color.white)
+            )
         )
 
         Column(
@@ -73,9 +78,12 @@ fun SearchScreen(onBackClick: () -> Unit) {
                 placeholder = { Text(stringResource(R.string.search_placeholder)) },
                 modifier = Modifier.fillMaxWidth(),
                 shape = RoundedCornerShape(8.dp),
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Text
+                ),
                 colors = OutlinedTextFieldDefaults.colors(
-                    focusedContainerColor = Color(0xFFE9EBEE),
-                    unfocusedContainerColor = Color(0xFFE9EBEE),
+                    focusedContainerColor = colorResource(R.color.search_field_bg),
+                    unfocusedContainerColor = colorResource(R.color.search_field_bg),
                     focusedBorderColor = Color.Transparent,
                     unfocusedBorderColor = Color.Transparent
                 )
